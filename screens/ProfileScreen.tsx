ad07beeb224e056036a2b7722401e04c9234a5dd
dylancl <dylan_cathelyn@hotmail.be>
2022-04-04 21:58:30 +0200

    Remove Expo default application files

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,34 +1,35 @@
-import {StyleSheet} from "react-native";
-import {Text, View} from "../components/Themed";
-import {RootTabScreenProps} from "../types";
-import {useAuthentication} from "../hooks/useAuthentication";
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Text, View} from '../components/Themed';
+import {RootTabScreenProps} from '../types';
+import {useAuthentication} from '../hooks/useAuthentication';
 
 export default function ProfileScreen({
-                                          navigation,
-                                      }: RootTabScreenProps<"TabOne">) {
-    const user = useAuthentication();
-    console.log(user);
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Profile Page</Text>
+	navigation,
+}: RootTabScreenProps<'TabOne'>) {
+	const user = useAuthentication();
+	console.log(user);
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Profile Page</Text>
 
-        </View>
-    );
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
-    },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: '80%',
+	},
 });
