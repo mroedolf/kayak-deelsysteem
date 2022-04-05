527b49824c2c5aae65c90b5361e19afc393a21bd
dylancl <dylan_cathelyn@hotmail.be>
2022-04-05 14:08:36 +0200

    Add .env.example file

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import './config/firebase';
@@ -6,17 +7,17 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
-    const isLoadingComplete = useCachedResources();
-    const colorScheme = useColorScheme();
+	const isLoadingComplete = useCachedResources();
+	const colorScheme = useColorScheme();
 
-    if (!isLoadingComplete) {
-        return null;
-    } else {
-        return (
-            <SafeAreaProvider>
-                <Navigation colorScheme={colorScheme}/>
-                <StatusBar/>
-            </SafeAreaProvider>
-        );
-    }
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+		return (
+			<SafeAreaProvider>
+				<Navigation colorScheme={colorScheme}/>
+				<StatusBar/>
+			</SafeAreaProvider>
+		);
+	}
 }
