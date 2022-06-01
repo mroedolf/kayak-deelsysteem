445643967e5472f61ae59f592dce10ce9adc6731
dylancl <dylan_cathelyn@hotmail.be>
2022-06-01 23:44:10 +0200

    Redesigned homescreen, small improvements

--- components/styles/blocks/KayakCard.ts
+++ components/styles/blocks/KayakCard.ts
@@ -1,28 +1,33 @@
 import styled from 'styled-components/native';
 
 export const Wrapper = styled.TouchableOpacity`
-	background-color: #fff;
-	padding: ${({ theme }) => theme.space.medium};
-	border-radius: ${({ theme }) => theme.sizes.small};
+	background-color: ${({ theme }) => theme.colors.primary};
+	border-radius: ${({ theme }) => theme.sizes.medium};
 	margin-bottom: ${({ theme }) => theme.space.large};
-	box-shadow: 8px 4px 4px rgba(200, 217, 250, 0.4);
 `;
 
 export const Title = styled.Text`
-	font-size: ${({ theme }) => theme.font.sizes.base};
+	font-size: ${({ theme }) => theme.font.sizes.xl}px;
 	font-weight: ${({ theme }) => theme.font.weights.bold};
 	color: ${({ theme }) => theme.colors.primary};
 `;
 
-export const ImageWrapper = styled.View`
-	width: 100%;
-	margin-bottom: ${({ theme }) => theme.space.medium};
-	background-color: ${({ theme }) => theme.colors.primary};
-	border-radius: ${({ theme }) => theme.sizes.small};
+export const TextWrapper = styled.View`
+	background-color: ${({ theme }) => theme.colors.light};
+	border-radius: ${({ theme }) => theme.sizes.medium};
+	margin: ${({ theme }) => theme.space.small}px;
+	padding: ${({ theme }) => theme.space.small}px
+		${({ theme }) => theme.space.medium}px;
+`;
+
+export const Subtitle = styled.Text`
+	font-size: ${({ theme }) => theme.font.sizes.sm};
+	font-weight: ${({ theme }) => theme.font.weights.bold};
+	color: ${({ theme }) => theme.colors.darkGrey};
 `;
 
 export const Image = styled.Image`
 	width: 100%;
-	height: 200px;
+	height: 100px;
 	resize-mode: contain;
 `;
